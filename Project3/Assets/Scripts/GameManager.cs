52575d2ef51d3aedc52e0e8a4e3537db38331949
LilyW3865 <li733414@ucf.edu>
2025-04-09 14:42:16 -0400

    Added UI Elements

--- Project3/Assets/Scripts/GameManager.cs
+++ Project3/Assets/Scripts/GameManager.cs
@@ -2,38 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
   /*  public GameObject WinMenu;
     public GameObject LoseMenu;
-    public GameObject PauseMenu;
+    public GameObject PauseMenu; */
     
     public TextMeshProUGUI HealthText;
-    public TextMeshProUGUI timerText;
-    public TextMeshProUGUI ObjectiveText; */
+    public TextMeshProUGUI TimerText;
+    public TextMeshProUGUI ObjectiveText; 
 
     private bool isPaused;
+    private bool PlayerAlive;
     private bool PlayerWon;
+
     private float Timer;
+    private int Minutes;
+    private float Seconds;
     PlayerCollisions PlayerCollisions;
     // Start is called before the first frame update
     void Start()
     {
+        PlayerAlive = true;
         isPaused = false;
         PlayerWon = false;
-       // Timer = 5f;
+        Timer = 120f;
         Time.timeScale = 1;
 
-      //  PlayerCollisions = GameObject.Find("Player").GetComponent<PlayerCollisions>();
+        PlayerCollisions = GameObject.Find("Player").GetComponent<PlayerCollisions>();
 
-       // ObjectiveText.gameObject.SetActive(true);  
+        ObjectiveText.gameObject.SetActive(true);  
     }
 
     // Update is called once per frame
     void Update()
     {
-       // HealthText.text = "x" + PlayerCollisions.Health;
+        HealthText.text = "Lives:" + PlayerCollisions.Health;
 
         if(Input.GetKeyDown(KeyCode.P) && PlayerWon == false)
         {
@@ -47,13 +53,15 @@ public class GameManager : MonoBehaviour
 
         if(!isPaused)
         {
-           // timerText.text = "Time:" + (int)Timer;
-           // Timer -= Time.deltaTime;
+            Minutes = (int) Timer / 60;
+            Seconds = (int) Timer % 60;
+            TimerText.text = "" + (int)Minutes + ":" + (int)Seconds;
+            Timer = Timer - Time.deltaTime;
 
-           /* if(Timer <= 0f)
+            if(Timer <= 0f)
             {
                 Lose();
-            } */
+            } 
 
         }  
     }
@@ -69,7 +77,9 @@ public class GameManager : MonoBehaviour
         Debug.Log("You won");
         PlayerWon = true;
         Time.timeScale = 0;
-       // ObjectiveText.gameObject.SetActive(false);
+        ObjectiveText.gameObject.SetActive(false);
+        TimerText.gameObject.SetActive(false);
+        HealthText.gameObject.SetActive(false);
       //  WinMenu.SetActive(true);
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;  
@@ -78,9 +88,12 @@ public class GameManager : MonoBehaviour
     public void Lose()
     {
         Debug.Log("You lost");
-       // ObjectiveText.gameObject.SetActive(false);
+        ObjectiveText.gameObject.SetActive(false);
+        TimerText.gameObject.SetActive(false);
+        HealthText.gameObject.SetActive(false);
       //  LoseMenu.SetActive(true);
 
+        PlayerAlive = false;
         Time.timeScale = 0;
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
@@ -91,7 +104,9 @@ public class GameManager : MonoBehaviour
         Debug.Log("Game Paused");
         Time.timeScale = 0;
         isPaused = true;
-       // ObjectiveText.gameObject.SetActive(false);
+        ObjectiveText.gameObject.SetActive(false);
+        TimerText.gameObject.SetActive(false);
+        HealthText.gameObject.SetActive(false);
        // PauseMenu.SetActive(true);
 
         Cursor.lockState = CursorLockMode.None;
@@ -103,11 +118,30 @@ public class GameManager : MonoBehaviour
         Debug.Log("Game Unpaused");
         Time.timeScale = 1;
         isPaused = false;
-       // ObjectiveText.gameObject.SetActive(true);
+        ObjectiveText.gameObject.SetActive(true);
+        TimerText.gameObject.SetActive(true);
+        HealthText.gameObject.SetActive(true);
        // PauseMenu.SetActive(false);
 
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+    }
+
+    private void Restart()
+    {
+        if(!PlayerAlive)
+        {
+            //Reload level
+           // SceneManager.LoadScene("");
+        }
+
+        if(PlayerWon)
+        {
+            //Load Menu
+           // SceneManager.LoadScene("");
+
+        }
+
     }
 
 
